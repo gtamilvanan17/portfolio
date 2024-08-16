ea85c337fe2f48b97255ce87cdfa869959be376f
Tamilvanan Gowran <121214873+gtamilvanan17@users.noreply.github.com>
2024-08-16 10:57:00 +0530

    hot-fix-2 (#6)

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 // import projectsData from "./assets/json/projects.json" assert { type: "json" };
 
+/*---------- Progress Bar Start --------*/
+// When the user scrolls the page, execute pageScroll function 
+window.onscroll = function() {
+  pageScroll();
+};
+
+function pageScroll() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("proBar").style.width = scrolled + "%";
+}
+/*---------- Progress Bar Start --------*/
+
 const reveal = {
   distance: "30px",
   easing: "ease-in",
